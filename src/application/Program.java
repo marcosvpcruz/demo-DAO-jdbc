6a8e6344bcc66314dd8cefdffe5b70b443b20f86
Marcos Vinicius <marcosvpcruz@yahoo.com.br>
2020-03-30 16:35:11 -0300

    FindById and Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,11 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department dep = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Marcos", "Marcos@gmail.com", new Date(), 3000.0, dep);
-		System.out.println(dep);
-		System.out.println(seller);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(" === TEST 1: Seller findById ===");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import java.util.List;
+
 import model.entities.Department;
 
 public interface DepartmentDao {
@@ -8,4 +10,5 @@ public interface DepartmentDao {
 	void update(Department obj);
 	void deleteById(Integer id);
 	Department findById(Integer id);
+	List<Department> findAll();
 }
